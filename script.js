dff734e28cb18c19f8ae8fe25ab6b4b10bda728f
ShoutingEagle <117582341+ShoutingEagle@users.noreply.github.com>
2023-08-03 12:05:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
     //Write your code here
-
+	const strong = document.getElementsByTagName("strong");
+    for(let i=0;i<strong.length;i++){
+        strong[i].classList.add("blue");
+    }
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+	const strong = document.getElementsByTagName("strong");
+    for(let i=0;i< strong.length;i++){
+         strong[i].classList.remove("blue");
+    }
     
 }
